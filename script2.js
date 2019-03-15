c0ac3574b8749cef7b35e649dfab3b1ad561cf99
Nishant <739833@cognizant.com>
2019-03-15 13:22:45 +0530

    object vs primitives

--- script2.js
+++ script2.js
@@ -8,6 +8,7 @@
 //
 //console.log(a);
 //console.log(b());
+
 //example 2
 if(true){
    var a=10;
@@ -16,3 +17,33 @@ if(true){
    }
 console.log(a);
 console.log(b);
+
+//example 3
+
+var a=10;
+var b= a;
+a=20; // it won't have effect on value of b
+console.log(a); // 20
+console.log(b);  //10
+
+
+var c= {
+    
+    name: 'john'
+};
+
+var d=c; // it meeans d and c point to same object
+c.name ='Nick';
+
+//so op will change in case of both references c and d
+//
+//result can be checked by typing window in console.
+//
+//now both c and d will have same value of name as nick.
+
+var c= {
+    
+    name: 'nishant'
+};
+
+//now there will be two different objects
