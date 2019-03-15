1f4a1377d6ec2ebec64c6b7dbbf03fba4157a7f9
Nishant <739833@cognizant.com>
2019-03-15 09:55:01 +0530

    while and do while demos

--- loopsdemo.js
+++ loopsdemo.js
@@ -15,3 +15,33 @@ for(var i=0;i<arr.length;i++){
        }
     console.log(arr[i]);
 }
+
+//while loop demo
+
+var i=0;
+
+while(i<=10){
+    i++;
+      console.log(i);
+}
+
+
+
+var colors =['white','red','green','blue'];
+             var i=0;
+while(i<colors.length){
+      i++;
+    console.log(colors[i]);
+      }
+
+//do while
+
+var colors2 =['white','red','green','blue'];
+
+var i=0;
+do{
+    console.log(colors2[i]);
+    i++;
+}while(i<colors2.length);
+
+
